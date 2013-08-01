ac3e2f82fc3e8fd287b2d9cb0c6feda14c9ccac0
Adries <frederic.maurer@udamail.fr>
2013-08-01 11:26:21 +0200

    34

--- mobile/www/annuaire/js/annuaire.js
+++ mobile/www/annuaire/js/annuaire.js
@@ -32,9 +32,9 @@ function makelist(json) {
 		else {
 			listelement = makelist.exact;			
 			for(i=0; i<n; i++) {
-				html+= '<li class=\"resultAnnuaire\"><h3>' + listelement[i].nom + ' ' + listelement[i].prenom + '</h3><img src=\"css/mail_green.png\">Courriel : <p><a href=\"mailto:' + listelement[i].mail + '\">' + listelement[i].mail + '</a></p>';
+				html+= '<li class=\"resultAnnuaire\"><h3>' + listelement[i].nom + ' ' + listelement[i].prenom + '</h3><img src=\"images/mail_green.png\">Courriel : <p><a href=\"mailto:' + listelement[i].mail + '\">' + listelement[i].mail + '</a></p>';
 				if(listelement[i].tel) {
-					html+= '<img src=\"css/phone.png\">TEL :<a href=\"tel:' + listelement[i].tel + '\">' + listelement[i].tel + '</a>';
+					html+= '<img src=\"images/phone.png\">TEL :<a href=\"tel:' + listelement[i].tel + '\">' + listelement[i].tel + '</a>';
 				}
 				html+='</li>';
 			}
